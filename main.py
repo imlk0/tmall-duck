9ff3e45372e978bf9cca58e7f5d4030d727b81a4
imlk <imlk.geek@gmail.com>
2020-11-14 21:39:47 +0800

    feat: add custom config file path support

--- main.py
+++ main.py
@@ -8,22 +8,25 @@ import notify
 import speak
 import os
 import worker
+import argparse
+
+
+project_dir = os.path.dirname(os.path.realpath(__file__))
+parser = argparse.ArgumentParser(description='tmall duck tool')
+parser.add_argument('-c', '--config', dest='config_file', action='store', default=project_dir+'/config.json', required=False, help='config file path, default is config.json')
+args = parser.parse_args()
+config = json.load(open(args.config_file, 'r'))
 
 print("--------- 触发打卡 ---------")
 print("当前系统时间：{}".format(strftime('%Y-%m-%d %H:%M:%S', localtime())))
 
-project_dir = os.path.dirname(os.path.realpath(__file__))
-config_private_file = project_dir + '/config_private.json'
-config_file = project_dir + '/config.json'
-if os.path.exists(config_private_file):
-    config = json.load(open(config_private_file, 'r'))
-else:
-    config = json.load(open(config_file, 'r'))
 
-tasks_info = fetch.fetch_tasks_info(config) # finished_all, days_of_checkin, schedule, tasks, ele_tasks.screenshot_as_png
+# finished_all, days_of_checkin, schedule, tasks, ele_tasks.screenshot_as_png
+tasks_info = fetch.fetch_tasks_info(config)
 # 输出首次获取任务结果
 msg = notify.build_task_info_msg(tasks_info)
 print(msg)
+
 
 if tasks_info[0] is None:
     # 获取失败
