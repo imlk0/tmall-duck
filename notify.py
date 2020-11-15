ad15fe8bb31f0f35c6783fd81723332e855517e3
imlk <imlk.geek@gmail.com>
2020-11-15 15:31:09 +0800

    refactor: rewite notify function

--- notify.py
+++ notify.py
@@ -24,54 +24,15 @@ def build_task_info_msg(tasks_info):
 
 
 def notify_owner(config, tasks_info):
-    access_token = get_access_token(
-        config['weixin']['corpid'], config['weixin']['secret'])
     if tasks_info[0] is None:
-        content = {
-            "touser": "@all",
-            "msgtype": "text",
-            "agentid": int(config['weixin']['agentid']),
-            "text": {
-                "content": "获取今日打卡信息失败\n时间：{}".format(strftime('%Y-%m-%d %H:%M:%S', localtime()))
-            }
-        }
-
+        notify_text_msg(config, "获取今日打卡信息失败\n时间：{}".format(
+            strftime('%Y-%m-%d %H:%M:%S', localtime())))
     else:
-        # pic_url = upload_pic(access_token, tasks_info[4])
         msg = build_task_info_msg(tasks_info)
-        content = {
-            "touser": "@all",
-            "msgtype": "text",
-            "agentid": int(config['weixin']['agentid']),
-            "text": {
-                "content": "天猫精灵打卡结束[{}]\n时间：{}\n{}".format('已完成' if tasks_info[0] else '未完成',
-                                                            strftime(
-                                                                '%Y-%m-%d %H:%M:%S', localtime()),
-                                                            msg)
-            }
-        }
-
-    resp = requests.post(
-        'https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=' + access_token, data=json.dumps(content))
-    j = resp.json()
-    if j['errcode'] != 0:
-        print("文字消息推送请求异常：", resp.json())
-
+        notify_text_msg(config, "天猫精灵打卡结束[{}]\n时间：{}\n{}".format(
+            '已完成' if tasks_info[0] else '未完成', strftime('%Y-%m-%d %H:%M:%S', localtime()), msg))
     if tasks_info[4] is not None:
-        media_id = upload_pic(access_token, tasks_info[4])
-        content = {
-            "touser": "@all",
-            "msgtype": "image",
-            "agentid": int(config['weixin']['agentid']),
-            "image": {
-                "media_id": media_id
-            }
-        }
-        resp = requests.post(
-            'https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=' + access_token, data=json.dumps(content))
-        j = resp.json()
-        if j['errcode'] != 0:
-            print("图片消息推送请求异常：", resp.json())
+        notify_picture_msg(config, tasks_info[4])
 
 
 def upload_pic(access_token, pic_bytes):
@@ -89,3 +50,40 @@ def get_access_token(corpid, corpsecret):
     }
     resp = requests.get('https://qyapi.weixin.qq.com/cgi-bin/gettoken', params)
     return resp.json()['access_token']
+
+
+def notify_text_msg(config, text: str):
+    access_token = get_access_token(
+        config['weixin']['corpid'], config['weixin']['secret'])
+    content = {
+        "touser": "@all",
+        "msgtype": "text",
+        "agentid": int(config['weixin']['agentid']),
+        "text": {
+            "content": text
+        }
+    }
+    resp = requests.post(
+        'https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=' + access_token, data=json.dumps(content))
+    j = resp.json()
+    if j['errcode'] != 0:
+        print("文字消息推送请求异常：", resp.json())
+
+
+def notify_picture_msg(config, pic):
+    access_token = get_access_token(
+        config['weixin']['corpid'], config['weixin']['secret'])
+    media_id = upload_pic(access_token, pic)
+    content = {
+        "touser": "@all",
+        "msgtype": "image",
+        "agentid": int(config['weixin']['agentid']),
+        "image": {
+            "media_id": media_id
+        }
+    }
+    resp = requests.post(
+        'https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=' + access_token, data=json.dumps(content))
+    j = resp.json()
+    if j['errcode'] != 0:
+        print("图片消息推送请求异常：", resp.json())
